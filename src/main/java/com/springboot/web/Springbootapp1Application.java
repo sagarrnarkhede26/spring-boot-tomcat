67724f022697eeec0720d7ee89a407d4fadf96c8
Sagar Narkhede <sagar.sanjay-narkhede@capgemini.com>
2022-09-06 09:58:32 +0530

    spring boot tomcat initial commit

---
+++ src/main/java/com/springboot/web/Springbootapp1Application.java
@@ -0,0 +1,14 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Springbootapp1Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootapp1Application.class, args);
+	}
+
+}
